bda81b99507c80f4ce74666c0e2f70e071af2e68
chunke2 <1005009818@qq.com>
2025-09-06 00:54:49 +0200

    debugging

--- cmake/toolchains/arm-gcc.cmake
+++ cmake/toolchains/arm-gcc.cmake
@@ -7,7 +7,7 @@ set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # STM32F746 (Cortex-M7 + single-precision FPU)
-set(MCPU_FLAGS "-mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16")
+#set(MCPU_FLAGS -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16)
 
 # add_compile_options(${MCPU_FLAGS} -ffunction-sections -fdata-sections -Wall -Wextra)
-add_link_options(${MCPU_FLAGS} -Wl,--gc-sections -Wl,--print-memory-usage --specs=nano.specs)
+#add_link_options(${MCPU_FLAGS} -Wl,--gc-sections -Wl,--print-memory-usage --specs=nano.specs)
